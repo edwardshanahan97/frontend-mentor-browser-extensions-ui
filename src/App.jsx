8164bdb83161c6a9458859caac919319cfb328ed
Edward Shanahan <312564962+edwardshanahan97@users.noreply.github.com>
2026-08-25 16:12:48 +0100

    Add Navbar component and toggle color mode using context

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import Navbar from "./components/Navbar/Navbar";
 import Filters from "./components/Filters/Filters";
 import ExtensionsList from "./components/ExtensionsList/ExtensionsList";
 import Container from "./components/Container/Container";
+import { useTheme } from "./context/ThemeContext/ThemeContext";
 
 const App = () => {
+  const { theme } = useTheme();
+
   return (
-    <main>
+    <main className="app" data-theme={theme}>
       <Container>
         <Navbar />
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import "./Navbar.css";
+import Logo from "../Logo/Logo";
+import SunIcon from "../../assets/images/icon-sun.svg";
+import MoonIcon from "../../assets/images/icon-moon.svg";
+import { useTheme } from "../../context/ThemeContext/ThemeContext";
 
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <nav onClick={toggleTheme} className="navbar">
+      <Logo />
+
+      <button className="navbar__toggle">
+        {theme === "dark" ? (
+          <img src={SunIcon} alt="Sun Icon" />
+        ) : (
+          <img src={MoonIcon} alt="Moon Icon" />
+        )}
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
