9aae03c9ff88c00808204075a709faa208fd6934
Edward Shanahan <312564962+edwardshanahan97@users.noreply.github.com>
2026-08-25 23:09:33 +0100

    Get user prefered color scheme

--- src/context/ThemeContext/ThemeContext.jsx
+++ src/context/ThemeContext/ThemeContext.jsx
@@ -3,7 +3,13 @@ import { createContext, useContext, useState } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+
+    return prefersDarkMode ? "dark" : "light";
+  });
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
